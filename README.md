4979eba52ec5cbd52395fab247661469c442bd9d
Myer921 <Myer@morz.org>
2016-08-10 01:37:21 +0800

    新开项目 	新文件：   LICENSE 	修改：     README.md 	新文件：   index.js 	新文件：   package.json

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # hexo-tag-dplayer
+感谢某豆子(dixyes)的移植，也感谢源项目https://github.com/grzhan/hexo-tag-aplayer
+
+源项目:hexo-tag-aplayer 作者grzhan
+
+本项目是将diygod的dplayer运行在hexo的插件
+
+即将单开项目
+---------------------------------------------
+
+
+
+Embed DPlayer([https://github.com/DIYgod/DPlayer](https://github.com/DIYgod/DPlayer)) in Hexo posts/pages.
+
+![plugin screenshot](https://static.morz.org/data/img/2016-05-24-07-31.png)
+
+
+
+## npm install
+
+	npm install hexo-tag-dplayer --save
+
+## Usage
+
+	{% dplayer key=value ... %}
+
+key can be 
+
+	'autoplay': will play automatic or not
+	'loop': will loop or not
+	'url': video source url
+	'pic': video cover picture
+	'api': DPlayer danmaku backend url
+	'id': see https://github.com/DIYgod/DPlayer
+	'token': see https://github.com/DIYgod/DPlayer
+
+for example:
+
+	{% dplayer "url=http://devtest.qiniudn.com/若能绽放光芒.mp4" "api=http://dplayer.daoapp.io" "pic=http://devtest.qiniudn.com/若能绽放光芒.png" "id=9E2E3368B56CDBB4" "loop=yes" "theme=#FADFA3" "autoplay=false" "token=tokendemo" %}
+
+## Customization
+
+You can modify variables `scriptDir`(default: "/assets/js/" ) and `styleDir`(default: "/assets/css/") in `index.js` according to your blog's directory structure.
+
+## Todo
+
+- [x] Publish it to the [hexo plugin list](https://hexo.io/plugins) and npm
+
+## LICENSE
+
+MIT
---
+++ index.js
@@ -0,0 +1,109 @@
+/**
+* hexo-tag-dplayer
+* https://github.com/NextMoe/hexo-tag-dplayer
+* Transplant recipients : dixyes
+* Integrated source protocol 
+* ---------------------------------
+* source item
+* hexo-tag-aplayer
+* https://github.com/grzhan/hexo-tag-aplayer
+* Copyright (c) 2016, grzhan
+* Licensed under the MIT license.
+* ---------------------------------
+* Syntax:
+*  {% dplayer key=value ... %}
+*/
+var fs = require('hexo-fs'),
+  util = require('hexo-util'),
+  path = require('path'),
+  counter = 0,
+  srcDir = path.dirname(require.resolve('dplayer')),
+  scriptDir = 'assets/js/',
+  styleDir = 'assets/css/',
+  dplayerScript = 'DPlayer.min.js',
+  dplayerStyle = 'DPlayer.min.css',
+  registers = [
+    [dplayerStyle, styleDir + dplayerStyle, path.join(srcDir, dplayerStyle)],
+    [dplayerScript, scriptDir + dplayerScript, path.join(srcDir, dplayerScript)],
+  ];
+
+for (var i = 0; i < registers.length; ++i) {
+  (function (i) {
+    var register = registers[i], regName = register[0],
+      pubPath = register[1], srcPath = register[2];
+    if(fs.existsSync(srcPath))
+        hexo.extend.generator.register(regName, function(locals) {
+          return {
+            path: pubPath,
+            data: function() {
+              return fs.createReadStream(srcPath);
+            }
+          };
+        });
+  })(i);
+}
+
+hexo.extend.filter.register('after_post_render', function(data) {
+  data.content =
+    (fs.existsSync(path.join(srcDir, dplayerStyle)) ? util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: '/' + styleDir + dplayerStyle }) : '') +
+    util.htmlTag('script', {src: '/' + scriptDir + dplayerScript}, " ") +
+    data.content;
+  return data;
+});
+
+// {% dplayer key=value ... %}
+hexo.extend.tag.register('dplayer', function(args) {
+  var  url = 'null', api = 'null', loop = false, autoplay = false, theme = "null", pic="null", did="null", token="null";
+    id = 'dplayer' + (counter++);
+    raw =  '<div id="'+ id + '" class="dplayer" style="margin-bottom: 20px;"></div>';
+    for (var i = 0; i < args.length; ++i) {
+        arg=args[i];
+        if(arg.split('=').length<2)
+            continue;
+        switch(arg.split('=')[0]){
+            case 'autoplay':
+                if(arg.split('=')[1]=='true'||arg.split('=')[1]=='yes'||arg.split('=')[1]=='1')
+                    autoplay = true;
+                break;
+            case 'loop':
+                if(arg.split('=')[1]=='true'||arg.split('=')[1]=='yes'||arg.split('=')[1]=='1')
+                    loop = true;
+                break;
+            case 'url':
+                url = arg.slice(arg.indexOf("=")+1);
+                break;
+            case 'pic':
+                pic = arg.slice(arg.indexOf("=")+1);
+                break;
+            case 'api':
+                api = arg.slice(arg.indexOf("=")+1);
+                break;
+            case 'id':
+                did = arg.slice(arg.indexOf("=")+1);
+                break;
+            case 'token':
+                token = arg.slice(arg.indexOf("=")+1);
+                break;
+        }
+    }
+
+  raw +=
+    '<script>var '+ id + ' = new DPlayer({'+
+        'element: document.getElementById("'+ id +'"),' +
+        'autoplay: ' + (autoplay ? 'true' : 'false') + ',' +
+        'loop: ' + (loop ? 'true' : 'false') +
+        (theme == 'null' ? '': ',theme: "' + theme + '"') +
+        ',video : {' +
+          'url: "'+ url + '"' +
+          (pic == 'null' ? '': ',pic: "'+ pic + '"') +
+        '}' +
+        (api == 'null' ? ' ':
+        ',danmaku : {' +
+          'api: "'+ api + '"' +
+          (did == 'null' ? '': ',id: "'+ did + '"') +
+          (token == 'null' ? '': ',token: "'+ token + '"') +
+        '}') +
+      '});' +
+    id + '.init();</script>';
+  return raw;
+});
